b2243b90ef013d8462db40d89684a88cd8817b7e
Kheyali Mitra <kheyali.mitra@finn.ai.com>
2021-01-17 11:14:15 -0800

    chore: added npm commands

--- server.js
+++ server.js
@@ -1,17 +1,13 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// var cookieParser = require('cookie-parser');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
