a38bacb00a71d16e6a9324ce7592078386fe6cfa
Kheyali Mitra <kheyali.mitra@finn.ai.com>
2021-01-17 17:42:43 -0800

    docs: swagger doc added

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 const { getStoreDetails, postStoreDetails }  = require('../handler/requestHandler');
 
 router.get('/store', function(req, res) {
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-
+// bodyParser = require("body-parser"),
+// swaggerJsdoc = require("swagger-jsdoc"),
+// swaggerUi = require("swagger-ui-express");
 var indexRouter = require('./routes/index');
 
 var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// app.set('view engine', 'jade');
 
 app.use('/', indexRouter);
 
@@ -26,6 +30,39 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+// /**
+//  * Swagger doc 
+//  */
+// const options = {
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Simple API with Swagger",
+//       version: "0.1.0",
+//       description:
+//         "This is a simple CR API application made with Express and documented with Swagger",
+//       license: {
+//         name: "MIT",
+//       },
+//       contact: {
+//         name: "Kheyali Mitra",
+//       },
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3000/store",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/index.js"],
+// };
+
+// const specs = swaggerJsdoc(options);
+// app.use(
+//   "/api-docs",
+//   swaggerUi.serve,
+//   swaggerUi.setup(specs)
+// );
 app.listen(3000, () => {
   console.log(`Listening on port 3000! ...`);
 });
